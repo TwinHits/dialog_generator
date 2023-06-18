e192e3fe2f7097a1de792bfed0aca13b922dccef
John Goben <TwinHits@gmail.com>
2023-06-18 19:31:36 -0400

    Added tests.

---
+++ test/cdk/dialogue_generator.test.ts
@@ -0,0 +1,16 @@
+import { describe, expect } from '@jest/globals';
+
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+
+import * as DialogueGenerator from '../../lib/dialogue_generator-stack'
+
+describe('CDK', () => {
+	test('Stack Created', () => {
+		const app = new cdk.App();
+		//const stack = new DialogueGenerator.DialogueGeneratorStack(app, 'MyTestStack');
+		//const template = Template.fromStack(stack);
+
+		//expect(template).toBeDefined();
+	});
+});
---
+++ test/main.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect } from '@jest/globals';
+
+describe('dialogue generator', () => {
+	test('tests run successfully', () => {
+		expect(true).toBeTruthy();
+	});
+});
