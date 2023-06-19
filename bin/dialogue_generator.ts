8d49eb0129ac3094271481286ae2f66f3d91a0fc
John Goben <TwinHits@gmail.com>
2023-06-18 22:22:22 -0400

    Changed tests to test and changed workflow name.

--- bin/dialogue_generator.ts
+++ bin/dialogue_generator.ts
@@ -4,5 +4,4 @@ import * as cdk from 'aws-cdk-lib';
 import { DialogueGeneratorStack } from '../lib/cdk_test-stack';
 
 const app = new cdk.App();
-    env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-});
+let env = { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION };
