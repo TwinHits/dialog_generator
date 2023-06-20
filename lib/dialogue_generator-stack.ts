844a08ece2877d3c07965d21ec820a860b310f11
John Goben <TwinHits@gmail.com>
2023-06-19 21:06:58 -0400

    Fixed lint command.

--- lib/dialogue_generator-stack.ts
+++ lib/dialogue_generator-stack.ts
@@ -1,8 +1,31 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+
+import { Construct } from "constructs";
 
 export class DialogueGeneratorStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-  }
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const dialogue_generator_lambda = new lambda.Function(this, "dialogue_generator_lambda", {
+            runtime: lambda.Runtime.NODEJS_18_X,
+            code: lambda.Code.fromAsset("src"),
+            handler: "handler",
+            environment: {
+                API_KEY: "KEY"
+            }
+        });
+
+        const api_gateway_service = new apigateway.RestApi(this, "widgets-api", {
+            restApiName: "Dialogue Generator Service",
+            description: "REST service for Dialogue Generator."
+        });
+
+        const post_dialog_integration = new apigateway.LambdaIntegration(dialogue_generator_lambda, {
+            requestTemplates: { "application/json": "{ \"statusCode\": \"200\" }" }
+        });
+
+        api_gateway_service.root.addMethod("POST", post_dialog_integration);
+    }
 }
