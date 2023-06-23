5e7b0dcdc17c67c6f9d20f3041f9f89ebcf5a39b
John Goben <TwinHits@gmail.com>
2023-06-23 15:19:48 -0400

    Changes to stack to try to fix not finding account issue.

--- bin/dialog_generator.ts
+++ bin/dialog_generator.ts
@@ -4,4 +4,4 @@ import * as cdk from "aws-cdk-lib";
 import { DialogGeneratorStack } from "../lib/dialog_generator-stack";
 
 const app = new cdk.App();
-new DialogGeneratorStack(app, "dialog-generator");
+new DialogGeneratorStack(app, "dialog_generator_stack",{});
