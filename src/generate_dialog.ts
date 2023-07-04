89f83ba74d835f4fb35ebf3f9a818daa81db00c6
John Goben <TwinHits@gmail.com>
2023-07-04 12:27:11 -0400

    Split handler from main function.

---
+++ src/generate_dialog.ts
@@ -0,0 +1,4 @@
+export const generate_dialog = (personality: string, facts: string[], length: number): string  => {
+    const dialog = "This is dialog";
+    return dialog;
+};
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
+import { generate_dialog } from "./generate_dialog";
+
 export const handler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  
-    return { 
-        statusCode: 200, 
-        body: "Hello world!", 
-    };
+    if (event.body) {
+        const body = JSON.parse(event.body);
+        const personality = body.personality;
+        const facts = body.facts;
+        const length = body.length;
+
+        const result = generate_dialog(personality, facts, length);
+         
+        return { 
+            statusCode: 200, 
+            body: result, 
+        };
+    } else {
+        return { 
+            statusCode: 400, 
+            body: "Missing Required Parameters", 
+        };
+    }
 };
