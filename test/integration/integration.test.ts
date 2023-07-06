7d0ae1dc632f1637477876ba1b07546bb4bcb37a
John Goben <TwinHits@gmail.com>
2023-07-05 21:39:30 -0400

    Fixed integration test requests format.

--- test/integration/integration.test.ts
+++ test/integration/integration.test.ts
@@ -1,9 +1,7 @@
 import "dotenv/config";
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 import { describe, expect, test } from "@jest/globals";
-
-import api_gateway_proxy_event from "../data/api_gateway_proxy_event";
 
 describe("endpoint", () => {
 
@@ -13,74 +11,99 @@ describe("endpoint", () => {
     });
 
     test("status code is 200", async () => {
-        const test_request = api_gateway_proxy_event;
-        test_request.body = JSON.stringify({
+        const test_request = {
             personality: "space captain",
             facts: [
                 "a fact",
             ],
             length: 255,
-        });
+        };
 
-        const actual = await axios.post(process.env.BASE_URL as string, test_request);
+        try {
+            const actual = await axios.post(process.env.BASE_URL as string, test_request);
 
-        expect(actual).toBeDefined();
-        expect(actual.status).toEqual(200);
+            expect(actual).toBeDefined();
+            expect(actual.status).toEqual(200);
+        } catch (error) {
+            console.log(error);
+            expect(error).not.toBeDefined();
+        }
     });
 
     test("body contains result", async () => {
-        const test_request = api_gateway_proxy_event;
-        test_request.body = JSON.stringify({
+        const test_request = {
             personality: "space captain",
             facts: [
                 "a fact",
             ],
             length: 255,
-        });
-        const actual = await axios.post(process.env.BASE_URL as string, test_request);
+        };
+       
+        try {
+            const actual = await axios.post(process.env.BASE_URL as string, test_request);
 
-        expect(actual).toBeDefined();
-        expect(actual.data).toEqual("This is dialog");
+            expect(actual).toBeDefined();
+            expect(actual.status).toEqual(200);
+            expect(actual.data).toEqual("This is dialog");
+        } catch (error) {
+            console.log(error);
+            expect(error).not.toBeDefined();
+        }
     });
 
     test("status code is 400 when 'personality' field is missing", async () => {
-        const test_request = api_gateway_proxy_event;
-        test_request.body = JSON.stringify({
+        const test_request = {
             facts: [
                 "a fact",
             ],
             length: 255,
-        });
+        };
 
-        const actual = await axios.post(process.env.BASE_URL as string, test_request);
-
-        expect(actual).toBeDefined();
-        expect(actual.status).toEqual(200);
+        try {
+            await axios.post(process.env.BASE_URL as string, test_request);
+        } catch (error) {
+            const actual = error as AxiosError;
+            expect(actual).toBeDefined();
+            expect(actual.response).toBeDefined();
+            expect(actual.response?.status).toEqual(400);
+        }
     });
 
     test("status code is 200 when 'facts' field is missing", async () => {
-        const test_request = api_gateway_proxy_event;
-        test_request.body = JSON.stringify({
+        const test_request = {
             personality: "space captain",
             length: 255,
-        });
-        const actual = await axios.post(process.env.BASE_URL as string, test_request);
+        };
 
-        expect(actual).toBeDefined();
-        expect(actual.data).toEqual("This is dialog");
+        try {
+            const actual = await axios.post(process.env.BASE_URL as string, test_request);
+
+            expect(actual).toBeDefined();
+            expect(actual.status).toEqual(200);
+            expect(actual.data).toEqual("This is dialog");
+        } catch (error) {
+            console.log(error);
+            expect(error).not.toBeDefined();
+        }
     });
 
     test("status code is 200 when 'length' field is missing", async () => {
-        const test_request = api_gateway_proxy_event;
-        test_request.body = JSON.stringify({
+        const test_request = {
             personality: "space captain",
             facts: [
                 "a fact",
             ],
-        });
-        const actual = await axios.post(process.env.BASE_URL as string, test_request);
+        };
 
-        expect(actual).toBeDefined();
-        expect(actual.data).toEqual("This is dialog");
+        try {
+            const actual = await axios.post(process.env.BASE_URL as string, test_request);
+
+            expect(actual).toBeDefined();
+            expect(actual.status).toEqual(200);
+            expect(actual.data).toEqual("This is dialog");
+        } catch (error) {
+            console.log(error);
+            expect(error).not.toBeDefined();
+        }
     });
 });
